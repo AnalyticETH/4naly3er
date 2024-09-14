90f7caebaf1336b0e219235f6ed59414ddeb1447
AnalyticETH <hello@analytic.lol>
2024-09-14 12:56:24 -0400

    add selfdestruct/timestamp from Pierregvx

---
+++ src/issues/M/selfdestruct.ts
@@ -0,0 +1,12 @@
+import { IssueTypes, RegexIssue } from '../../types';
+
+const issue: RegexIssue = {
+  regexOrAST: 'Regex',
+  type: IssueTypes.M,
+  title: 'Used selfdestruct',
+  impact: 'Any contract that depends on another smart contract must account for the fact that the other can vanish at any time. Moreover, the SELFDESTRUCT opcode is deprecated and is recommended to no longer be used.',
+  regex: /selfdestruct\([a-zA-Z0-9\.\(\)]+\)/g
+  ,
+};
+
+export default issue;
