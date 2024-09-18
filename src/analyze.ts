13fa7f27737dc8368b7f42a60664aac65d069636
Matthew Pilsbury <matthew.pilsbury@coinbase.com>
2024-09-18 19:42:18 -0400

    diagnose missing issues in SARIF

--- src/analyze.ts
+++ src/analyze.ts
@@ -5,11 +5,90 @@ import * as fs from 'fs'; // Import the fs module
 // Titles for different issue types
 const issueTypesTitles = {
   GAS: 'Gas Optimizations',
-  NC: 'Non Critical Issues',
+  NC: 'Informational Issues',
   L: 'Low Issues',
   M: 'Medium Issues',
   H: 'High Issues',
 };
+
+//// Support functions
+
+// Function to convert a string to Pascal-case
+const toPascalCase = (str: string) => {
+  return str
+    .replace(/[^a-zA-Z0-9 ]/g, '') // Remove non-alphanumeric characters
+    .replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()) // Capitalize each word
+    .replace(/\s+/g, ''); // Remove spaces
+};
+
+  const sanitizeFileName = (fileName: string) => {
+    return fileName.replace(/^\.?\//, '');
+  };
+
+  // Function to sanitize file names by removing leading "." and "/"
+  const sanitizeFileNames = (fileName: string) => {
+    return fileName.replace(/^\.?\//, '');
+  };
+
+  // Remove the "fileContent" property from each instance in the analyze array
+  const analyzeWithoutFileContent = analyze.map(({ issue, instances }) => ({
+    issue,
+    instances: instances.map(({ fileName, line, endLine }) => ({ fileName, line, endLine }))
+  }));
+
+
+  // Function to map issue types to SARIF severity levels
+  const mapSeverity = (type: string) => {
+    switch (type) {
+      case 'NC':
+        return 'note';
+      case 'G':
+        return 'note';
+      case 'L':
+        return 'warning';
+      case 'M':
+        return 'warning';
+      case 'H':
+        return 'error';
+      default:
+        return 'note';
+    }
+  };
+
+  const mapSecuritySeverity = (type: string) => {
+    switch (type) {
+      case 'G':
+        return '0.0';
+      case 'NC':
+        return '2.5';
+      case 'L':
+        return '5.0';
+      case 'M':
+        return '7.5';
+      case 'H':
+        return '10.0';
+      default:
+        return '10.0';
+    }
+  };
+
+  // Function to map issue types to descriptive tags
+  const mapTag = (type: string) => {
+    switch (type) {
+      case 'NC':
+        return 'Informational';
+      case 'G':
+        return 'Gas Optimization';
+      case 'L':
+        return 'Low';
+      case 'M':
+        return 'Medium';
+      case 'H':
+        return 'High';
+      default:
+        return 'Unknown';
+    }
+  };
 
 /**
  * @notice Runs the given issues on files and generate the report markdown string
@@ -83,153 +162,6 @@ const analyze = (files: InputType, issues: Issue[], githubLink?: string): string
       analyze.push({ issue, instances });
     }
   }
-
-  /** SARIF Generation Section */
-
-// Function to convert a string to Pascal-case
-const toPascalCase = (str: string) => {
-  return str
-    .replace(/[^a-zA-Z0-9 ]/g, '') // Remove non-alphanumeric characters
-    .replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()) // Capitalize each word
-    .replace(/\s+/g, ''); // Remove spaces
-};
-
-const sanitizeFileName = (fileName: string) => {
-  return fileName.replace(/^\.?\//, '');
-};
-
-// Function to sanitize file names by removing leading "." and "/"
-const sanitizeFileNames = (fileName: string) => {
-  return fileName.replace(/^\.?\//, '');
-};
-
-// Remove the "fileContent" property from each instance in the analyze array
-const analyzeWithoutFileContent = analyze.map(({ issue, instances }) => ({
-  issue,
-  instances: instances.map(({ fileName, line, endLine }) => ({ fileName, line, endLine }))
-}));
-
-// Function to map issue types to SARIF severity levels
-const mapSeverity = (type: string) => {
-  switch (type) {
-    case 'NC':
-      return 'note';
-    case 'G':
-      return 'note';
-    case 'L':
-      return 'warning';
-    case 'M':
-      return 'warning';
-    case 'H':
-      return 'error';
-    default:
-      return 'note';
-  }
-};
-
-const mapSecuritySeverity = (type: string) => {
-  switch (type) {
-    case 'G':
-      return '0.0';
-    case 'NC':
-      return '2.5';
-    case 'L':
-      return '5.0';
-    case 'M':
-      return '7.5';
-    case 'H':
-      return '10.0';
-    default:
-      return '10.0';
-  }
-};
-
-// Function to map issue types to descriptive tags
-const mapTag = (type: string) => {
-  switch (type) {
-    case 'NC':
-      return 'Informational';
-    case 'G':
-      return 'Gas Optimization';
-    case 'L':
-      return 'Low';
-    case 'M':
-      return 'Medium';
-    case 'H':
-      return 'High';
-    default:
-      return 'Unknown';
-  }
-};
-
-  // Convert the JSON data to SARIF format
-  const sarif = {
-    version: "2.1.0",
-    $schema: "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
-    runs: [
-      {
-        tool: {
-          driver: {
-            name: "4naly3er",
-            fullName: "4naly3er Static Smart Contract Code Analyzer",
-            informationUri: "https://github.com/AnalyticETH/4naly3er",
-            version: "0.2", // Update the version number        
-            rules: analyze.map((item, index) => ({
-              id: `rule${index + 1}`,
-              name: item.issue.title,
-              shortDescription: {
-                text: item.issue.title
-              },
-              fullDescription: {
-                text: item.issue.description || "No description provided."
-              },              
-              helpUri: "https://github.com/AnalyticETH/4naly3er/blob/analytic/detectors.md",
-              help: {
-                text: item.issue.description || "No description provided."
-              },
-              properties: {
-                tags: [mapTag(item.issue.type)], // Map issue type to descriptive tag
-                "security-severity": mapSecuritySeverity(item.issue.type), // Map issue type to SARIF severity level
-                "problem.severity": mapSecuritySeverity(item.issue.type) // Map issue type to SARIF severity level
-              }
-            }))
-          }
-        },
-        automationDetails: {
-          id: "4naly3er"
-        },
-        results: analyze.flatMap((item, index) =>
-          item.instances.map(instance => ({
-            ruleId: `rule${index + 1}`,
-            message: {
-              text: item.issue.title
-            },
-            locations: [
-              {
-                physicalLocation: {
-                  artifactLocation: {
-                    uri: sanitizeFileNames(instance.fileName), // Sanitize the file name to remove leading "."
-                  },
-                  region: {
-                    startLine: instance.line,
-                    startColumn: 1
-                  }
-                }
-              }
-            ],
-            partialFingerprints: {
-              primaryLocationLineHash: `${sanitizeFileNames(instance.fileName)}:${instance.line}` // Sanitize file name to remove leading "."
-            },
-            level: mapSeverity(item.issue.type), // Map issue type to SARIF severity level
-          }))
-        )
-      }
-    ]
-  };
-
-  // Write the SARIF data to a file called report.sarif.json
-  fs.writeFileSync('report.sarif.json', JSON.stringify(sarif, null, 2), 'utf-8');
-
 
   /** Summary Section */
   let c = 0; // Counter for issue instances
@@ -311,6 +243,76 @@ const mapTag = (type: string) => {
     }
     result += `\n`;
   }
+
+  /** SARIF Generation Section */
+
+  // Convert the JSON data to SARIF format
+  const sarif = {
+    version: "2.1.0",
+    $schema: "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
+    runs: [
+      {
+        tool: {
+          driver: {
+            name: "4naly3er",
+            fullName: "4naly3er Static Smart Contract Code Analyzer",
+            informationUri: "https://github.com/AnalyticETH/4naly3er",
+            version: "0.2", // Update the version number        
+            rules: analyze.map((item, index) => ({
+              id: `rule${index + 1}`,
+              name: item.issue.title,
+              shortDescription: {
+                text: item.issue.title
+              },
+              fullDescription: {
+                text: item.issue.description || "No description provided."
+              },              
+              helpUri: "https://github.com/AnalyticETH/4naly3er/blob/analytic/detectors.md",
+              help: {
+                text: item.issue.description || "No description provided."
+              },
+              properties: {
+                tags: [mapTag(item.issue.type)], // Map issue type to descriptive tag
+                "security-severity": mapSecuritySeverity(item.issue.type), // Map issue type to SARIF severity level
+                "problem.severity": mapSecuritySeverity(item.issue.type) // Map issue type to SARIF severity level
+              }
+            }))
+          }
+        },
+        automationDetails: {
+          id: "4naly3er"
+        },
+        results: analyze.flatMap((item, index) =>
+          item.instances.map(instance => ({
+            ruleId: `rule${index + 1}`,
+            message: {
+              text: item.issue.title
+            },
+            locations: [
+              {
+                physicalLocation: {
+                  artifactLocation: {
+                    uri: sanitizeFileNames(instance.fileName), // Sanitize the file name to remove leading "."
+                  },
+                  region: {
+                    startLine: instance.line,
+                    startColumn: 1
+                  }
+                }
+              }
+            ],
+            partialFingerprints: {
+              primaryLocationLineHash: `${sanitizeFileNames(instance.fileName)}:${instance.line}` // Sanitize file name to remove leading "."
+            },
+            level: mapSeverity(item.issue.type), // Map issue type to SARIF severity level
+          }))
+        )
+      }
+    ]
+  };
+
+  // Write the SARIF data to a file called report.sarif.json
+  fs.writeFileSync('report.sarif.json', JSON.stringify(sarif, null, 2), 'utf-8');
 
   return result; // Return the generated report
 };
